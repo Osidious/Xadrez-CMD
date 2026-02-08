8913b32e666de093a6e4633e9eb140bd398fca86
Osidious <gabriel73ren@gmail.com>
2026-02-08 16:43:44 -0300

    Criando a classe  ChessPosition

---
+++ Xadrez-CMD/Controle/ChessPosition.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Tabuleiro;
+
+namespace Controle {
+    internal class ChessPosition {
+        public char Line { get; set; }
+        public int Column { get; set; }
+
+        public ChessPosition(char line, int column) {
+            Line = line;
+            Column = column;
+        }
+
+        public Position toPosition() {
+            return new Position(8 - Column,Line - 'a');
+        }
+
+        public override string ToString() {
+            return $"{Line}{Column}";
+        }
+    }
+}
--- Xadrez-CMD/Program.cs
+++ Xadrez-CMD/Program.cs
@@ -5,14 +5,20 @@ using Tabuleiro;
 namespace Xadrez_CMD {
     class Program {
         static void Main(string[] args) {
-            try {
+            /*try {
                 Board board = new Board(8, 8);
                 Screen.setBoard(board);
                 Screen.printBoard(board);
             }
             catch (BoardException e) {
                 Console.WriteLine(e.Message);
-            }
+            }*/
+
+            ChessPosition chessNotation = new ChessPosition('a', 8);
+            Console.WriteLine(chessNotation);
+
+            Console.WriteLine(chessNotation.toPosition());
+            
         }
 
         
